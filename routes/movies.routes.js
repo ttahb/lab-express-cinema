fa9f2bb648bc106c11397241b50d982e66a7548e
ttahb <bhattvijay69@hotmail.com>
2023-12-09 13:45:10 +0530

    all iterations without bonus

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -5,11 +5,25 @@ router.get('/movies', (req, res, next)=>{
     Movie.find()
         .then(movies => {
             console.log('Movies from db', movies);
-            res.render('movies',{ movies})
+            res.render('movies',{ movies});
         })
         .catch(error => {
-            console.log('Error while getting books from the DB!', error)
+            console.log('Error while getting movies from the DB!', error);
 
+            next(error);
+        });
+});
+
+
+router.get('/movies/:id', (req, res, next)=>{
+    const movieId = req.params.id;
+    Movie.findOne({_id: movieId})
+        .then(movie => {
+            console.log("movie", movie);
+            res.render('movie.hbs', movie);
+        })
+        .catch(error=> {
+            console.log('Error while getting movie detail from db!', error);
             next(error);
         });
 });
